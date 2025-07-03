2722696e17c2804d5e44fdd6eea46aeb7fcad692
Tagwolf <tagwolf116@gmail.com>
2025-07-03 19:52:39 +0200

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,98 @@
+window.wolfProfiles = [
+    {
+        name: "BimsHingefallt",
+        oldNames: ["BimsHingefallt_", "BimsYT"],
+        avatar: "https://mc-heads.net/avatar/BimsHingefallt/80.png",
+        cosmetics: [
+            { name: "1 Jahr Kuchen", icon: "🎂" },
+            { name: "Wolf Ohren", icon: "🐺" },
+            { name: "Krone", icon: "👑" },
+            { name: "Brille", icon: "🕶️" }
+        ]
+    },
+    {
+        name: "MagicWolfMC",
+        oldNames: ["MagicWolf", "WolfiHD"],
+        avatar: "https://mc-heads.net/avatar/MagicWolfMC/80.png",
+        cosmetics: [
+            { name: "1 Jahr Kuchen", icon: "🎂" },
+            { name: "Wolf Cape", icon: "🧣" },
+            { name: "Wolf Ohren", icon: "🐺" }
+        ]
+    },
+    {
+        name: "Steve",
+        oldNames: [],
+        avatar: "https://mc-heads.net/avatar/Steve/80.png",
+        cosmetics: [
+            { name: "1 Jahr Kuchen", icon: "🎂" },
+            { name: "Classic Cape", icon: "🦸" }
+        ]
+    },
+    {
+        name: "NightwolfLG",
+        oldNames: ["Nightwolf", "Nighty"],
+        avatar: "https://mc-heads.net/avatar/NightwolfLG/80.png",
+        cosmetics: [
+            { name: "1 Jahr Kuchen", icon: "🎂" },
+            { name: "Wolf Ohren", icon: "🐺" },
+            { name: "Krone", icon: "👑" },
+            { name: "Brille", icon: "🕶️" },
+            { name: "Wolf Cape", icon: "🧣" },
+            { name: "Flügel", icon: "🪽" },
+            { name: "Krallenhandschuhe", icon: "🧤" },
+            { name: "Wolf Schwanz", icon: "🦊" },
+            { name: "Wolf Maske", icon: "🎭" },
+            { name: "Wolf Hoodie", icon: "🧥" },
+            { name: "Wolf Schuhe", icon: "👟" },
+            { name: "Wolf Rucksack", icon: "🎒" },
+            { name: "Wolf Brille", icon: "🕶️" },
+            { name: "Wolf Krone", icon: "👑" },
+            { name: "Wolf Umhang", icon: "🦺" },
+            { name: "Wolf Kette", icon: "📿" },
+            { name: "Wolf Ohrring", icon: "🦻" },
+            { name: "Wolf Ring", icon: "💍" },
+            { name: "Wolf Tattoo", icon: "🦁" },
+            { name: "Wolf Armband", icon: "📿" },
+            { name: "Wolf Stirnband", icon: "🎀" },
+            { name: "Wolf Cape Gold", icon: "🥇" },
+            { name: "Wolf Cape Silber", icon: "🥈" },
+            { name: "Wolf Cape Bronze", icon: "🥉" },
+            { name: "Wolf Krone Gold", icon: "👑" },
+            { name: "Wolf Krone Silber", icon: "👑" }
+        ]
+    },
+    {
+        name: "DLS_GoldGeist",
+        oldNames: ["GoldGeist", "DLS_GG"],
+        avatar: "https://mc-heads.net/avatar/DLS_GoldGeist/80.png",
+        cosmetics: [
+            { name: "1 Jahr Kuchen", icon: "🎂" },
+            { name: "Wolf Ohren", icon: "🐺" },
+            { name: "Krone", icon: "👑" },
+            { name: "Brille", icon: "🕶️" },
+            { name: "Wolf Cape", icon: "🧣" },
+            { name: "Flügel", icon: "🪽" },
+            { name: "Krallenhandschuhe", icon: "🧤" },
+            { name: "Wolf Schwanz", icon: "🦊" },
+            { name: "Wolf Maske", icon: "🎭" },
+            { name: "Wolf Hoodie", icon: "🧥" },
+            { name: "Wolf Schuhe", icon: "👟" },
+            { name: "Wolf Rucksack", icon: "🎒" },
+            { name: "Wolf Brille", icon: "🕶️" },
+            { name: "Wolf Krone", icon: "👑" },
+            { name: "Wolf Umhang", icon: "🦺" },
+            { name: "Wolf Kette", icon: "📿" },
+            { name: "Wolf Ohrring", icon: "🦻" },
+            { name: "Wolf Ring", icon: "💍" },
+            { name: "Wolf Tattoo", icon: "🦁" },
+            { name: "Wolf Armband", icon: "📿" },
+            { name: "Wolf Stirnband", icon: "🎀" },
+            { name: "Wolf Cape Gold", icon: "🥇" },
+            { name: "Wolf Cape Silber", icon: "🥈" },
+            { name: "Wolf Cape Bronze", icon: "🥉" },
+            { name: "Wolf Krone Gold", icon: "👑" },
+            { name: "Wolf Krone Silber", icon: "👑" }
+        ]
+    }
+];
